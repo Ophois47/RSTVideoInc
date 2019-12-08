50eade9a347fb6b35002b6e8f8a859bc5d98bb4b
Ophois47 <48365899+Ophois47@users.noreply.github.com>
2019-12-07 20:43:55 -0500

    3

--- RSTVideoInc/Controllers/MoviesController.cs
+++ RSTVideoInc/Controllers/MoviesController.cs
@@ -1,23 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
 using System.Web.Mvc;
 using RSTVideoInc.Models;
 using RSTVideoInc.ViewModels;
 
-namespace RSTVideoInc.Controllers
+namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
-        // GET: /Movies/Random
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Happy Scrappy Hero Pup" },
+                new Movie { Id = 2, Name = "NAVY SEALs" }
+            };
+        }
+
+        // GET: Movies/Random
         public ActionResult Random()
         {
-            var movie = new Movie() {Name = "Happy Scrappy Hero Pup"};
+            var movie = new Movie() { Name = "Shrek!" };
             var customers = new List<Customer>
             {
-                new Customer {Name = "Customer 1"},
-                new Customer {Name = "Customer 2"}
+                new Customer { Name = "Customer 1" },
+                new Customer { Name = "Customer 2" }
             };
 
             var viewModel = new RandomMovieViewModel
