1169a42b32d7d6997429dbf1a319414918b81b59
Ophois47 <48365899+Ophois47@users.noreply.github.com>
2019-12-11 20:29:48 -0500

    Movie Forms

--- RSTVideoInc/Models/Movie.cs
+++ RSTVideoInc/Models/Movie.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Drawing.Printing;
-using System.Linq;
-using System.Web;
 
 namespace RSTVideoInc.Models
 {
@@ -15,13 +11,18 @@ namespace RSTVideoInc.Models
         [StringLength(255)]
         public string Name { get; set; }
 
-        [Required]
         public Genre Genre { get; set; }
+
+        [Display(Name = "Genre")]
+        [Required]
         public byte GenreId { get; set; }
 
         public DateTime DateAdded { get; set; }
+
+        [Display(Name = "Release Date")]
         public DateTime ReleaseDate { get; set; }
 
+        [Display(Name = "Number in Stock")]
         public byte NumberInStock { get; set; }
     }
 }
