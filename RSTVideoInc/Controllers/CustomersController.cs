c126a1a9b1b3a9ab6f90dab7f272a647186eea1f
Ophois47 <48365899+Ophois47@users.noreply.github.com>
2019-12-18 18:42:12 -0500

    Form Errors

--- RSTVideoInc/Controllers/CustomersController.cs
+++ RSTVideoInc/Controllers/CustomersController.cs
@@ -26,6 +26,7 @@ namespace RSTVideoInc.Controllers
             var membershipTypes = _context.MembershipTypes.ToList();
             var viewModel = new CustomerFormViewModel
             {
+                Customer = new Customer(),
                 MembershipTypes = membershipTypes
             };
 
